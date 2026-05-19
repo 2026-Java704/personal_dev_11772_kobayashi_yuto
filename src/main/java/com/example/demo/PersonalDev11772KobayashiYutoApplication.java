162e345dee270bc667ddec2618f83eed1311759d
student <student@FJ-1025>
2026-05-19 11:37:54 +0900

    新規作成１

---
+++ src/main/java/com/example/demo/PersonalDev11772KobayashiYutoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalDev11772KobayashiYutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalDev11772KobayashiYutoApplication.class, args);
+	}
+
+}
