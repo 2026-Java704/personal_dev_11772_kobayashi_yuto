fc5cb24abf60696b935b1ff17e893d85b13695e8
student <student@FJ-1025>
2026-05-26 16:44:14 +0900

    オプション機能の完成とCSSの完成

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
 -- users テーブルにデータを挿入するクエリ
 INSERT INTO users ( email, name, password)
 VALUES
+('tanaka@aaa.com', '田中太郎', 'test123'),
 ('kobayashi@aaa.com', '小林悠斗', 'test789');
-
 
 --categories テーブルにデータを挿入するクエリ
 INSERT INTO categories(name) VALUES('運動');
@@ -13,5 +13,6 @@ INSERT INTO categories(name) VALUES('その他');
 -- tasks テーブルにデータを挿入するクエリ
 INSERT INTO tasks (user_id, category_id,title, closing_date, progress, memo)
 VALUES
-(1, 1, '筋トレ', '2026/5/18', 0, '腕立て、背筋、腹筋をそれぞれ100回する');
+(1, 1, '筋トレ', '2026/5/18', 0, '腕立て、背筋、腹筋をそれぞれ100回する'),
+(2, 1, '数学', '2026/5/18', 0, '腕立て、背筋、腹筋をそれぞれ100回する');
 
