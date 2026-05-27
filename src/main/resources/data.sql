65b5862847849a319c1ff632b6a4fc9fc870da80
student <student@FJ-1025>
2026-05-27 17:59:11 +0900

    テーブルの修正変更

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,7 @@
 -- users テーブルにデータを挿入するクエリ
 INSERT INTO users ( email, name, password)
 VALUES
-('tanaka@aaa.com', '田中太郎', 'test123'),
-('kobayashi@aaa.com', '小林悠斗', 'test789');
+('KobayashiYuto@gmail.com', '小林悠斗', 'test456');
 
 --categories テーブルにデータを挿入するクエリ
 INSERT INTO categories(name) VALUES('運動');
@@ -13,6 +12,5 @@ INSERT INTO categories(name) VALUES('その他');
 -- tasks テーブルにデータを挿入するクエリ
 INSERT INTO tasks (user_id, category_id,title, closing_date, progress, memo)
 VALUES
-(1, 1, '筋トレ', '2026/5/18', 0, '腕立て、背筋、腹筋をそれぞれ100回する'),
-(2, 1, '数学', '2026/5/18', 0, '腕立て、背筋、腹筋をそれぞれ100回する');
+(1, 2, '数学', '2026/5/28', 0, '事前に予習をしておくようにする');
 
